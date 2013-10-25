68b906eda7ff34ab1c7ed46bd6771a0bfc79f0c5
anthony cantor <ar.cantor@gmail.com>
2013-10-24 20:43:09 -0600

    implemented account database from stdin
    havent implemented hashing for passwords yet

--- haschat.hs
+++ haschat.hs
@@ -1,8 +1,32 @@
 -- adapted from tutorial http://www.catonmat.net/blog/simple-haskell-tcp-server/
 import Network (listenOn, withSocketsDo, accept, PortID(..), Socket)
 import System.Environment (getArgs)
-import System.IO (hSetBuffering, hGetLine, hPutStrLn, BufferMode(..), Handle)
+import System.IO (
+  hClose,
+  hSetBuffering,
+  hGetLine,
+  hPutStrLn,
+  hPutStr,
+  BufferMode(..),
+  Handle
+  )
 import Control.Concurrent (forkIO)
+import qualified Data.Map as Map
+import Safe
+
+data Account = Account String String deriving (Eq, Show, Read)
+type AccountDB = Map.Map String String
+
+accountDB :: [String] -> AccountDB
+accountDB lns =
+  foldl add_account Map.empty accounts
+  where
+    add_account mp (Account user passhash) = Map.insert user passhash mp
+    accounts = 
+      let
+        fn Nothing xs  = xs
+        fn (Just x) xs = x:xs
+      in foldr (fn . readMay) [] lns
 
 parseArgs :: (Num a) => [String] -> a
 parseArgs []     = 9393
@@ -12,26 +36,40 @@ main :: IO ()
 main = withSocketsDo $ do
   args <- getArgs
   let port = parseArgs args
+  putStrLn "importing account data from stdin"
+  account_data <- getContents
+  let account_db = accountDB $ lines account_data
+  putStrLn $ "account db" ++ (show account_db) 
   sock <- listenOn $ PortNumber port
   putStrLn $ "listening on " ++ (show port)
-  sockHandler sock
+  sockHandler sock account_db
 
-sockHandler :: Socket -> IO ()
-sockHandler sock = do
+sockHandler :: Socket -> AccountDB -> IO ()
+sockHandler sock account_db = do
   (handle, _, _) <- accept sock
   hSetBuffering handle NoBuffering
-  forkIO $ commandProcessor handle
-  sockHandler sock
+  forkIO $ login handle account_db
+  sockHandler sock account_db
 
-commandProcessor :: Handle -> IO ()
-commandProcessor handle = do
-  line <- hGetLine handle
-  let cmd = words line
-  case (head cmd) of
-    ("echo") -> echoCommand handle cmd
-    _ -> do hPutStrLn handle "unknown command"
-  commandProcessor handle
+validatePass :: String -> String -> String -> Bool
+validatePass user pass passhash = pass == passhash
 
-echoCommand :: Handle -> [String] -> IO ()
-echoCommand handle cmd = do
-  hPutStrLn handle (unwords $ tail cmd)
+login :: Handle -> AccountDB -> IO ()
+login handle account_db = do
+  hPutStr handle "i can has user? "
+  user <- hGetLine handle
+  hPutStr handle "i can has password? "
+  pass <- hGetLine handle
+
+  case (Map.lookup user account_db) of
+    Nothing       -> do hPutStrLn handle "invalid login"
+    Just passhash -> do 
+      if (validatePass user pass passhash)
+        then do run handle user
+        else do hPutStrLn handle "invlaid login"
+
+  hClose handle
+
+run :: Handle -> String -> IO ()
+run handle user = do
+  hPutStrLn handle $ "welcome " ++ user ++ ", u can haschat now."
